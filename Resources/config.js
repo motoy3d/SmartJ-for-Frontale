0ff3c132bc7a99e1ec7286b5d68b8c5a25f93592
motoy3d <motoy3d@gmail.com>
2014-12-15 04:08:20 +0900

    first commit

---
+++ Resources/config.js
@@ -0,0 +1,27 @@
+exports.config = {
+    teamId: 'frontale'
+    ,hashtag: 'frontale'
+    ,teamName: '川崎'
+    ,appName: 'スマートJ for 川崎フロンターレ'
+    ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id912618358?l=ja&ls=1&mt=8'
+    ,androidAppUrl: 'https://play.google.com/store/apps/details?id=frontale.mylife'
+    ,color: '#1298ef'
+    ,tintColor: '#000'
+    ,dbName: 'frontale.mylife'
+    //ニュースのソースURL
+    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    //日程・結果のソースURL
+    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=frontale&season="
+    //設定、メッセージのソースURL
+    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=frontale"
+    ,newsEntriesPerPage: 50
+    ,googleAnalyticsTrackingId: 'UA-30928840-1'
+    //nend(iPhone)
+    ,nendSpotIdIPhoneIcon: 223281
+    ,nendApiKeyIPhoneIcon: '9ff6481849b2c8984342564111758c5b7f11cef1'
+    ,nendSpotIdIPhoneBanner: 223279
+    ,nendApiKeyIPhoneBanner: '7b414b41df8cb6590eac0e528d7eaa4edcef5bd8'
+    //nend(Android)
+    ,nendSpotIdAndroid: 223286
+    ,nendApiKeyAndroid: '22d155359a537302321960b318da52ba6f51af14'
+};
