bac5ad8bd293e219b2e8e617ff0d3198aa76c632
motoy3d <motoy3d@gmail.com>
2016-10-03 22:12:38 +0900

    iOS ver 1.0.3

--- Resources/config.js
+++ Resources/config.js
@@ -2,18 +2,40 @@ exports.config = {
     teamId: 'frontale'
     ,hashtag: 'frontale'
     ,teamName: '川崎'
+    ,teamNameFull: '川崎フロンターレ'
     ,appName: 'スマートJ for 川崎フロンターレ'
     ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id912618358?l=ja&ls=1&mt=8'
     ,androidAppUrl: 'https://play.google.com/store/apps/details?id=frontale.mylife'
+    ,backgroundColor: 'black'
+	,visitedBgColor : '#ddd'
+    ,mainTextColor: 'white'
     ,color: '#1298ef'
     ,tintColor: '#000'
+    ,standingsBgcolor: '#1298ef'
+    ,standingsTeamFontColor: 'white'
+    ,resultsDetailBtnBgColorActive: '#333'
+    ,resultsDetailBtnColorActive: 'white'
+    ,resultsDetailBtnBgColorInactive: '#202020'
+    ,resultsDetailBtnColorInactive: '#444'
     ,dbName: 'frontale.mylife'
+    //URLベース
+    ,urlBase: 'https://smartj.mobi/redsmylife/'
     //ニュースのソースURL
     ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
     //日程・結果のソースURL
     ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=frontale&season="
+    //順位表のソースURL
+    ,standingsUrl: 'http://sub0000499082.hmk-temp.com/redsmylife/standings.json'
     //設定、メッセージのソースURL
     ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=frontale"
+    //動画のソースURL
+    ,videoUrl: "http://sub0000499082.hmk-temp.com/redsmylife/video.json?teamId=frontale"
+    //報告URL
+    ,reportUrl: "https://smartj.mobi/redsmylife/report.json?teamId=frontale"
+    //利用規約URL
+    ,rulesUrl: "https://smartj.mobi/redsmylife/rules.jsp?appName="
+    //開発元メールアドレス
+    ,developerMail: "motoy3d+smartj@gmail.com"
     ,newsEntriesPerPage: 50
     ,googleAnalyticsTrackingId: 'UA-30928840-1'
     //nend(iPhone)
@@ -24,4 +46,6 @@ exports.config = {
     //nend(Android)
     ,nendSpotIdAndroid: 223286
     ,nendApiKeyAndroid: '22d155359a537302321960b318da52ba6f51af14'
+    ,nendSpotIdAndroidBanner: 223285
+    ,nendApiKeyAndroidBanner: '7f549a4643f2cdf5aee1bd3e3066c7e1033de47a'
 };
